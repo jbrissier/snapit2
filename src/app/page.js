244907a391c2d088f3370c07e42e0b209df2c964
Jochen Brissier <jochen@Jochens-MacBook-Pro-2.local>
2023-06-02 10:34:09 +0200

    add next-auth with github to the project

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export  default async function Home() {
 
         <div>
           <h2 className="text-2xl mb-3">Events</h2>
-          <ul class="list-disc">
+          <ul className="list-disc">
           {events.map((event) =>
             (<li>
             <Link href={`/${event.id}`}>{event.name}</Link></li>)
